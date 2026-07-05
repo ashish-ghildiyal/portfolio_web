14542ea32beaa04d915f82a87af8b7013f0ddcbd
Ashish <ashish.ghildiyal@xelba.io>
2026-07-05 21:04:54 +0530

    correct pdf file path

--- src/components/DocumentCard.jsx
+++ src/components/DocumentCard.jsx
@@ -11,7 +11,7 @@ import PdfIcon from "./PdfIcon";
 export default function DocumentCard({ label, file }) {
   return (
     <a
-      href={file}
+      href={`${import.meta.env.BASE_URL}file`}
       target="_blank"
       rel="noopener noreferrer"
       className="doc-card"
